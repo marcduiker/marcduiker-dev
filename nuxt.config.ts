9394ff9540fc49877809eb373efed842d9f0fe70
Marc Duiker <marcduiker@users.noreply.github.com>
2023-05-07 22:16:07 +0200

    Add nuxtjs/sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/alpine',
-  modules: ['@nuxtjs/plausible'],
+  modules: ['@nuxtjs/plausible', '@nuxtjs/sitemap'],
 
   plausible: {
     domain: 'marcduiker.dev'
