c50eb8ce4ed8ad7ecc15b5267ea33dc614ca8245
Marc Duiker <4191277+marcduiker@users.noreply.github.com>
2026-07-11 11:45:47 +0200

    Update view on link
    Signed-off-by: Marc Duiker <4191277+marcduiker@users.noreply.github.com>

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -92,8 +92,8 @@ export const tests = {
   }
 };
 export const viewRepo = {
-  // this is for the view/edit on github link. The value in the package.json will be pulled in.
+  // this is for the view/edit on Codeberg link. The value in the package.json will be pulled in.
   allow: true,
-  infoText: 'View this page on GitHub'
+  infoText: 'View this page on Codeberg'
 };
 export const easteregg = true;
